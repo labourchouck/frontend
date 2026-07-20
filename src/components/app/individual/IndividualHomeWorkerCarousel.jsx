c60b706672f183bdf9382af5be5d3ec0f001ae07
Rashi Jaiswal <rashijaiswal6655@gmail.com>
2026-07-20 15:59:16 +0530

    upload image from admin panel

--- src/components/app/individual/IndividualHomeWorkerCarousel.jsx
+++ src/components/app/individual/IndividualHomeWorkerCarousel.jsx
@@ -35,7 +35,7 @@ export function IndividualHomeWorkerCarousel({
                 type="button"
                 onClick={onEmptyAction}
                 className="rounded-xl-
-                . bg-[#22a159] px-4 py-2 text-base font-bold text-white shadow-md hover:bg-[#1e8d4e] transition-colors"
+                . bg-[#22a159] rounded-xl px-4 py-2 text-base font-bold text-white shadow-md hover:bg-[#1e8d4e] transition-colors"
               >
                 {emptyAction}
               </button>
